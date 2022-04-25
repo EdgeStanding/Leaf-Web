18465cd7e05c9a570ba57360c866e8932661d2b4
iVampireSP.com <im@ivampiresp.com>
2022-04-25 11:17:21 +0800

    Update: Login

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,42 +1,37 @@
 <template>
   <h1 v-text="title"></h1>
   <n-space vertical>
-    <n-input
-      v-model:value="token"
-      type="password"
-      name="token"
-      placeholder="输入 Token 以继续"
-      :disabled="buttonDisabled"
-    />
     <div>
-      <n-button type="primary" @click="connect" :disabled="buttonDisabled"
-        >连接</n-button
+      <n-button
+        v-if="show"
+        type="primary"
+        @click="toLeaf()"
+        :disabled="buttonDisabled"
+        >前往 Leaf</n-button
       >
-      &nbsp;
-      <n-button type="primary" @click="toLeaf" :disabled="buttonDisabled"
-        >回到 Leaf</n-button
-      >
+      <p v-else>我们正在将你重定向到 Edge.st Leaf...</p>
     </div>
   </n-space>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-import { NSpace, NInput, NButton } from "naive-ui";
+import { onMounted, ref } from "vue";
+import { NSpace, NButton } from "naive-ui";
 import store from "../../plugins/store";
 import http from "../../api/http";
 import router from "../../plugins/router";
 import api from "../../config/api";
 
 const token = ref("");
-const title = ref("连接到 Edge.st");
+const show = ref(false);
+const title = ref("连接到 Leaf");
 const buttonDisabled = ref(false);
 
 // const user = store.state.user
 
 const connect = () => {
   buttonDisabled.value = true;
-  title.value = "正在验证 Token...";
+  title.value = "正在验证登录...";
   store.commit("updateToken", token.value);
   http
     .get("/user")
@@ -46,13 +41,14 @@ const connect = () => {
       title.value = "嗨, " + res.data.name;
       buttonDisabled.value = false;
 
+      show.value = true;
       router.push("/");
     })
     .catch((err) => {
       store.commit("updateToken", {
         token: null,
       });
-      title.value = "Token 验证失败，请确认 Token 是否正确。";
+      title.value = "无法验证登录，将在几秒钟后重试。";
       buttonDisabled.value = false;
     });
   token.value = "";
@@ -64,6 +60,9 @@ const query = router.currentRoute.value.query;
 if (query.token != null) {
   token.value = query.token as string;
   connect();
+} else {
+  show.value = false;
+  setTimeout(toLeaf, 1000)
 }
 
 function toLeaf() {
